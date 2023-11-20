d14bc3659b5e9295780ee46750de27c99e813243
SegundoPaladines <segundopaladines@gmail.com>
2023-11-19 22:06:24 -0500

    primer deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { AppRouter } from './Router';
+import { Container, Grid} from '@mui/material';
+import { TitleComponent } from '../../components/titulo';
+import React, { useState } from "react";
+import { PersonDataComponent } from '../../components/datos_persona';
+import { ScoreForm } from '../../components/formulario_puntaje';
 
 function App() {
+
+  const [score, setScore] = useState<number>(0);
+
+  const handleScore = (newScore: number) =>{
+    setScore(newScore);
+  }
+  
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Container 
+      maxWidth="xl"
+      sx={{
+        mt:12
+      }}
+    >
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        gap={4}
+        sx={{
+          minWidth: "100hv"
+        }}
+      >
+        <Grid item xs={12}>
+          <TitleComponent 
+            title="Registrar Resultados Pruebas Saber Pro"
+            color="white"
+            divider
+            dividerColor="green"
+          />
+        </Grid>
+        <Grid item 
+          xs={12}
+        >
+          <PersonDataComponent
+            title="Puntaje Global"
+            name="Samuel Hernandez"
+            profesion="Ingeniero de Sistemas"
+            score={score}
+            img="https://pbs.twimg.com/profile_images/1066477945076752384/6cgqAOkb_400x400.jpg"
+          />
+        </Grid>
+        <Grid item 
+          xs={12}
+        >
+          <ScoreForm 
+            onSocreChangue={handleScore}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
